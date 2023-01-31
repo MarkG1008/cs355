71bdb5e33bb03741c5d054b0d8a156ba1d3b9175
MarkG1008 <123514938+MarkG1008@users.noreply.github.com>
2023-01-31 18:11:15 -0500

    Assignment2.html

---
+++ script.js
@@ -0,0 +1,17 @@
+const mybtn = document.getElementById('myBtn');
+mybtn.addEventListener('click',buttonClicked);
+
+function buttonClicked(e){
+    document.getElementById("title").className="hover";
+
+
+}
+
+const mybtn2 = document.getElementById('myBtn2');
+mybtn.addEventListener('click',buttonClicked);
+
+function buttonClicked(e){
+    document.getElementById("title").className="hover";
+
+
+}
